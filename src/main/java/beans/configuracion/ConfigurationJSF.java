9398365b775025c4d36cef215249505b29b3b89f
Sebastian Quiroz <sebastian.122@hotmail.com>
2019-11-25 14:36:53 -0500

    Configuracion y primeros Beans Creados

---
+++ src/main/java/beans/configuracion/ConfigurationJSF.java
@@ -0,0 +1,19 @@
+
+package beans.configuracion;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import static javax.faces.annotation.FacesConfig.Version.JSF_2_3;
+
+/**
+ *
+ * @author ADMIN
+ */
+@FacesConfig(
+        //Activa el CDI build-in beans
+        version = JSF_2_3
+)
+@ApplicationScoped
+public class ConfigurationJSF {
+    
+}
